45f932124e056e04ef967bc7da8f242e807083f5
Clickipedia <mysimplyclick@gmail.com>
2023-04-26 18:31:46 +0530

    about

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
         <div className='mb-20'>
             <div className='h-screen grid place-items-center'>
 
-                <h1 className='text-5xl mb-32' > <span className='font-bold' >WE ARE</span> PASSIONATE TEAM</h1>
+                <h1 className='md:text-5xl text-2xl mb-32' > <span className='font-bold' >WE ARE</span> PASSIONATE TEAM</h1>
 
             </div>
-            <div className=' bg-[#1e1e45] 2xl:px-[20rem] lg:px-20 py-20 space-y-10'>
+            <div className=' bg-[#1e1e45] 2xl:px-[20rem] lg:px-20 px-5 py-20 space-y-10'>
 
                 <p>
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
@@ -31,29 +31,29 @@ const About = () => {
                 <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)} >
 
 
-                </ScrollTrigger>
                     <div className='2xl:w-[85rem] 2xl:-ms-[3.5rem] px-32 py-10 counting-section text-center grid grid-cols-3' >
                         <div>
-                            <h1 className='text-4xl font-semibold'>
+                            <h1 className='md:text-4xl text-xl font-semibold'>
                                 {counterOn && <CountUp start={0} end={3250} duration={1.2} delay={0} />}+
                             </h1>
-                            <p>Happy Customers</p>
+                            <p className='md:text-base text-xs'>Happy Customers</p>
                         </div>
                         <div>
-                            <h1 className='text-4xl font-semibold'>
+                            <h1 className='md:text-4xl text-xl font-semibold'>
                                 {counterOn && <CountUp start={0} end={1290} duration={1.2} delay={0} />}+
                             </h1>
-                            <p>Passionate Makeup Artist</p>
+                            <p className='md:text-base text-xs'>Passionate Makeup Artist</p>
                         </div>
                         <div>
-                            <h1 className='text-4xl font-semibold'>
+                            <h1 className='md:text-4xl text-xl font-semibold'>
                                 {counterOn && <CountUp start={0} end={1500} duration={1.2} delay={0} />}+
                             </h1>
-                            <p>Clinic Arround The World</p>
+                            <p className='md:text-base text-xs'>Clinic Arround The World</p>
                         </div>
 
                     </div>
 
+                </ScrollTrigger>
 
 
 
